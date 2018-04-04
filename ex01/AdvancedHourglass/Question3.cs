cf1c99255aa273b8814e2b842e6d6a507108b5be
lermanori <lermanori@gmail.com>
2018-04-04 16:33:48 +0300

    4.4.18 changes and thoughts
    there was a probloem with the way we thought last time we cant use the 5 lines hourglass because the right-side spacings. so in order to achieve the necessery functionality i chnaged the beginner hourglass function to create a sand clock by parameter and we'll refrence this function in advanced hourglass whos file isnt finished!.
    the testing frame work is built and working ill add the files .
    tested on the hour glass for inputs 0-100  also test files well be submitted.

--- ex01/AdvancedHourglass/Question3.cs
+++ ex01/AdvancedHourglass/Question3.cs
@@ -9,26 +9,50 @@ using System;
             bool validInput;
             string outputMessage, goodInputMessage, badInputMessage;
             string needOldHourglass, needSpacing;
+            string topPartOfHourGlass, bottomPartOfHourGlass;
             int inputNumber;
 
             badInputMessage = "input is invalid. Input must be a number!";
 
             userInput = Console.ReadLine();
             validInput = int.TryParse(userInput, out inputNumber);
+            inputNumber = inputNumber % 2 == 0 ? --inputNumber : inputNumber;// we chose to decrement one in a manner of paired input;
             needOldHourglass = inputNumber < 5 ? string.Empty : BegginersHourglass.Question2.hourGlass;
             needSpacing = inputNumber < 5 ? string.Empty : Environment.NewLine;
+            topPartOfHourGlass = addMissingSpaceToSimpleHourGlassToMakeHimAdvAnced(inputNumber);
+            bottomPartOfHourGlass = reverseOrderOfStringForHourGlasses(topPartOfHourGlass);
+            //0 - new hrgls top, 1 - newline , 2 - q2 hrgls  3 - new hrgls bottom
+           goodInputMessage = string.Format("{0}{1}{2}{1}{3}", "%top", needSpacing, needOldHourglass, "%bottom");//we cant use the @ (wysiwyg) because of the usage of simpleHourGlass string because if we dont need it we will have blank line that because of @ cant be nothing and have to be a new line 
 
-            // 0 - new hrgls top, 1 - newline , 2 - q2 hrgls  3 - new hrgls bottom
-            goodInputMessage = string.Format("{0}{1}{2}{1}{3}", "%top" , needSpacing, needOldHourglass, "%bottom");
-            goodInputMessage = string.Format(
-@"{0}
-{1}
-{2}", "%top", needOldHourglass, "%bottom");
+
+
+
+            //            goodInputMessage = string.Format(
+            //@"{0}
+            //{1}
+            //{2}", "%top", needOldHourglass, "%bottom");
 
             outputMessage = validInput ?  goodInputMessage : badInputMessage;
 
             Console.WriteLine(outputMessage);
             Console.ReadLine();
+        }
+
+        private static string reverseOrderOfStringForHourGlasses(string topPartOfHourGlass)
+        {
+            
+        }
+
+        private static string addMissingSpaceToSimpleHourGlassToMakeHimAdvAnced(int inputNumber)
+        {
+            string result = string.Empty;
+            for(int i= inputNumber;i>5; i-=2)
+            {
+                for (int j = 0; j < i; j++)
+                    string.Concat("*");
+                string.Concat(Environment.NewLine);
+            }
+            return result;
         }
     }
 }
--- ex01/BegginersHourglass/Question2.cs
+++ ex01/BegginersHourglass/Question2.cs
@@ -3,17 +3,53 @@
     using System;
     public class Question2
     {
-        public static string hourGlass =
-@"*****
- ***
-  *
- ***
-*****";
+//        public static string hourGlass =
+//@"*****
+// ***
+//  *
+// ***
+//*****";
 
         public static void Main()
         {
+            string hourGlass = createHourglass();
             Console.WriteLine(hourGlass);
             Console.ReadLine();
+        }
+        public static string createHourglass(int i_RequestedHeight=5,int i_rightSpacing=0)
+        {
+            string hourGlass =  string.Empty;
+            int spacingInsideHourGlass = 0;
+            i_RequestedHeight = i_RequestedHeight % 2 == 0 ? --i_RequestedHeight : i_RequestedHeight;//our chice for paired input
+            for (int j = i_RequestedHeight;j > 0;j-=2)
+            {
+                hourGlass += string.Format("{0}{1}{0}{2}", createStringFromCharAndNumber(spacingInsideHourGlass++,' ')//here is where spacing is advanced every iteration
+                                                        , createStringFromCharAndNumber(j,'*')
+                                                        , Environment.NewLine
+                                                        );
+            }
+            spacingInsideHourGlass-=2;//beacuse of first row already printed and we dont wont to duplicate it-spacingWise;
+            // at this certion point if we can reverse the array without first line will save the next loop;
+            
+            for (int j = 3; j <= i_RequestedHeight; j += 2)//beacuse of first row already printed and we dont wont to duplicate it-AstrixWise;
+            {
+                hourGlass += string.Format("{0}{1}{0}{2}", createStringFromCharAndNumber(spacingInsideHourGlass--, ' ')
+                                                        , createStringFromCharAndNumber(j, '*')
+                                                        , Environment.NewLine
+                                                        );
+            }
+
+
+            return hourGlass;
+        }
+        public static string createStringFromCharAndNumber(int i_numberOfapperences,char i_requestedChar='*')
+        {
+            string result = string.Empty;
+            for(int i=0; i<i_numberOfapperences;i++)
+            {
+                result += i_requestedChar;
+            }
+            return result;
         }
     }
 }
